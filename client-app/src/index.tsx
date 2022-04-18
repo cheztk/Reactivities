cc233e09e7f4c182d046bf038b6342f1146b6bbd
Chezt Kengchon <ckengchon@gmail.com>
2022-04-18 16:51:39 +0700

    End of section 5

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './app/layout/styles.css';
+import App from './app/layout/App';
 
-ReactDOM.render(
+const container = document.getElementById('root')!;
+const root = ReactDOM.createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
