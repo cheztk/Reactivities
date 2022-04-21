c09c5a4a584d348005d08f8138584903a4b4b837
Chezt Kengchon <ckengchon@gmail.com>
2022-04-21 15:20:52 +0700

    End of section 6

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,12 +15,14 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean
 }
 
 export default function ActivityDashboard({activities, 
     selectedActivity, 
     selectActivity, 
     deleteActivity,
+    submitting,
     cancelSelectedActivity,
     editMode,
     openForm,
@@ -33,6 +35,7 @@ export default function ActivityDashboard({activities,
                activities={activities} 
                selectActivity={selectActivity}
                deleteActivity={deleteActivity}
+               submitting={submitting}
                />
             </Grid.Column>
             <Grid.Column width='6'>
@@ -46,7 +49,8 @@ export default function ActivityDashboard({activities,
                 <ActivityForm
                 closeForm={closeForm}
                 activity={selectedActivity}
-                createOrEdit={createOrEdit}/>
+                createOrEdit={createOrEdit}
+                submitting={submitting}/>
                 
                 }
                 
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -7,9 +7,9 @@ import App from './app/layout/App';
 const container = document.getElementById('root')!;
 const root = ReactDOM.createRoot(container);
 root.render(
-  <React.StrictMode>
+  
     <App />
-  </React.StrictMode>
+  
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
